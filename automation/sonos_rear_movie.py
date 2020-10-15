82d5ca8cea7a4235e38f4fb4901ebf36baa38812
Jake Iwen <iwenx006@umn.edu>
2020-10-15 01:09:33 -0500

    new stuff

--- automation/sonos_rear_movie.py
+++ automation/sonos_rear_movie.py
@@ -1,9 +1,7 @@
 import soco
 from soco.discovery import by_name
 
-# soco.discovery.any_soco().partymode()
-
-by_name("vanFront").partymode()
-by_name("vanFront").mute = True
+by_name("vonFront").partymode()
+by_name("vonFront").mute = True
 by_name("vonMid").mute = True
 by_name("vonRear").volume = 90
