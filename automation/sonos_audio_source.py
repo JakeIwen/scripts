ead98e29ebe3fb4298f637a4f29c48b6032680a9
Jake Iwen <iwenx006@umn.edu>
2020-11-16 05:13:00 -0700

    stash

--- automation/sonos_audio_source.py
+++ automation/sonos_audio_source.py
@@ -6,21 +6,23 @@ from soco.discovery import by_name
 device_name =  sys.argv[1]
 audio_source = sys.argv[2]
 
-device = by_name(device_name) or by_name(device_name + "2")
-print(device)
+def sonos_audio_source(name, source):
+    device = by_name(name) or by_name(name + "2")
+    print(device)
 
-if audio_source == "optical":
-    mid = by_name("vonMid")
-    mid.switch_to_tv()
-    if device_name == "vonMid":
-        exit()
-    mid.partymode()
-    mid.group.mute = True
-    
-elif audio_source == "line":
-    device.switch_to_line_in(by_name("vonFront"))
-    
-device.mute = False
-device.volume = 90
-device.play()
+    if source == "optical":
+        mid = by_name("vonMid")
+        mid.switch_to_tv()
+        if name == "vonMid":
+            exit()
+        mid.partymode()
+        mid.group.mute = True
+        
+    elif source == "line":
+        device.switch_to_line_in(by_name("vonFront"))
+        
+    device.mute = False
+    device.volume = 90
+    device.play()
 
+sonos_audio_source(device_name, audio_source)
