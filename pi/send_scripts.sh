7316cae5f482da9b89fa217a2b56310e655c62fd
Jake Iwen <iwenx006@umn.edu>
2020-11-16 05:13:00 -0700

    stash

--- pi/send_scripts.sh
+++ pi/send_scripts.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+dsc=~/dev/scripts
 
-scp -r ~/dev/scripts/pi/scripts/ pi@192.168.6.103:/home/pi/
-scp  ~/dev/scripts/pi/.bashrc/ pi@192.168.6.103:/home/pi/.bashrc
+find $dsc/automation -type f -name "*.py" -exec cp {} $dsc/pi/scripts/python \;
+scp -r $dsc/pi/scripts/ pi@192.168.6.103:/home/pi/
+scp  $dsc/pi/.bashrc/ pi@192.168.6.103:/home/pi/.bashrc
+
