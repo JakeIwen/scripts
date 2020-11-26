d5ffd5abef148d3dc883cb936cd1e32d542a6348
Jake Iwen <iwenx006@umn.edu>
2020-11-26 05:45:53 -0700

    stash

--- pi/scripts/rsync_schedule.sh
+++ pi/scripts/rsync_schedule.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
-SEEGAYTE_MOUNTED=$(mount | awk '/seegayte/ {print $6}' | grep "rw")
-  if [ $SEEGAYTE_MOUNTED ]; then
-    echo "SEEGAYTE_MOUNTED: $SEEGAYTE_MOUNTED";
-    date;
-    rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/seegayte/pi_backup/;
-    
-    MP_MOUNTED=$(mount | awk '/movingparts/ {print $6}' | grep "rw")
-    echo "MP_MOUNTED: $MP_MOUNTED";
-    if [ $MP_MOUNTED ]
-    then rsync -avH --delete-during --delete-excluded /mnt/movingparts /mnt/seegayte/mp_backup/;
-    else echo "Movingparts drive not available or not writable";
-    fi
- else echo "Seegayte drive not available or not writable";
+# SEEGAYTE_MOUNTED=$(mount | awk '/seegayte/ {print $6}' | grep "rw")
+#   if [ $SEEGAYTE_MOUNTED ]; then
+#     echo "SEEGAYTE_MOUNTED: $SEEGAYTE_MOUNTED";
+#     date;
+#     rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/seegayte/pi_backup/;
+# 
+#     MP_MOUNTED=$(mount | awk '/movingparts/ {print $6}' | grep "rw")
+#     echo "MP_MOUNTED: $MP_MOUNTED";
+#     if [ $MP_MOUNTED ]
+#     then rsync -avH --delete  -during --delete-excluded /mnt/movingparts /mnt/seegayte/mp_backup/;
+#     else echo "Movingparts drive not available or not writable";
+#     fi
+#  else echo "Seegayte drive not available or not writable";
+#  fi
+ 
+ MP_MOUNTED=$(mount | awk '/movingparts/ {print $6}' | grep "rw")
+ echo "MP_MOUNTED: $MP_MOUNTED";
+ if [ $MP_MOUNTED ]
+ then rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/movingparts/pi_backup/;
+ else echo "Movingparts drive not available or not writable";
  fi
--- pi/send_scripts.sh
+++ pi/send_scripts.sh
@@ -6,7 +6,6 @@ find $dsc/automation -type f -name "*.py" -exec cp {} $dsc/pi/scripts/python/ \;
 
 scp -r $dsc/pi/scripts/ pi@192.168.6.103:/home/pi/
 scp  $dsc/pi/.bashrc/ pi@192.168.6.103:/home/pi/.bashrc
-scp  $dsc/pi/smb.conf pi@192.168.6.103:/home/pi/smb.conf 
 scp  $dsc/pi/sns.sh/ pi@192.168.6.103:/home/pi/sns.sh
 
 rm -R $dsc/pi/scripts/python/
