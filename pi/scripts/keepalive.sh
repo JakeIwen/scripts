5473c287b781c26c5e7f556c9bdd41a2b4db8083
Jake Iwen <iwenx006@umn.edu>
2020-12-08 11:23:38 -0700

    Stash

--- pi/scripts/keepalive.sh
+++ pi/scripts/keepalive.sh
@@ -2,5 +2,10 @@
 
 tasks=`ps ax`
 
-bash /home/pi/scripts/internet_switches.sh tasks
+bash /home/pi/scripts/internet_switches.sh
+
+if [[ "$(ps ax)" != *'python3 server.py'* ]]  &> /dev/null 
+then nohup python3 /home/pi/NativCast/server.py &
+fi
+
 
