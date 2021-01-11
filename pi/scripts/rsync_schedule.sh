1dddaacdc3ebb6e1ead9d358f134882ee00331a2
Jake Iwen <iwenx006@umn.edu>
2021-01-11 09:45:09 -0800

    stash

--- pi/scripts/rsync_schedule.sh
+++ pi/scripts/rsync_schedule.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
- 
- MP_MOUNTED=$(mount | awk '/movingparts/ {print $6}' | grep "rw")
- echo "MP_MOUNTED: $MP_MOUNTED";
- if [ $MP_MOUNTED ]
- then rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/movingparts/pi_backup/;
- else echo "Movingparts drive not available or not writable";
- fi
+date;
+
+SEEGAYTE_MOUNTED=$(mount | awk '/seegayte/ {print $6}' | grep "rw")
+MP_MOUNTED=$(mount | awk '/movingparts/ {print $6}' | grep "rw")
+
+[ ! $SEEGAYTE_MOUNTED ] && echo "Seegayte not available/writable" && exit 0
+sudo rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/seegayte/pi_backup/
+
+[ ! $MP_MOUNTED ] && echo "MP not available/writable" && exit 0
+sudo rsync -avH --delete-during --delete-excluded /mnt/movingparts /mnt/seegayte/mp_backup/;
