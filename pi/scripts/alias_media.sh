b17ee194cfa88ecb74c5d5ea89f60020bdc72d7d
Jake Iwen <iwenx006@umn.edu>
2021-02-01 22:25:22 -0700

    stash

--- pi/scripts/alias_media.sh
+++ pi/scripts/alias_media.sh
@@ -4,12 +4,14 @@ media_group_links() {
   ext="$1"
   regex="$2"
   folder="$3"
-  keys="${regex}Will1869|10\.?Bit|DTS|HMAX|IMAX|MA|VhsRip|x0r|iNTERNAL|True-HD|(1080|720)(p|i)|Xvi|HD|AC3|AAC|REPACK|REMUX|PRiCK|AVC|Atmos|EPSiLON|HC|AMZN|HEVC|Blu(R|r)ay|(BR|WEB|web)(Rip)?|NF|DDP?\+?|(x|X|H|h)\.?26[4-5]"
+  keys="${regex}Will1869|10\.?Bit|DTS|DL|NTb|ION10|MeGusta|METCON|strife|SDC|hdtv|CiNEFiLE|PRoDJi|EVO|POIASD|WiKi|HMAX|IMAX|MA|VhsRip|x0r|iNTERNAL|True-HD|(1080|720)(p|i)|Xvi|HD|AC3|AAC|REPACK|REMUX|PRiCK|AVC|d3g|Atmos|EPSiLON|HC|AMZN|HEVC|Blu(R|r)ay|(BR|WEB|web)(Rip)?|NF|DDP?\+?|(x|X|H|h)\.?26[4-5]"
   pattern="(\.|-)($keys)(.?(5.1)|(2.0))?-?\w*-?(?=(\.|-))|\[.*\]|$ext$"
   find "$folder" -not -path '*/\.*' -not -ipath '*sample*' -type f -iname "*.$ext" | while read pth
   do
-    [ -e "$pth" ] || continue
-    title=`basename "$pth" | perl -pe "s~$pattern~~g" | perl -pe "s/(-| |,)/./g" | perl -pe "s|\.+|.|g" | perl -pe "s/(.|-)$//g"`
+    [ -e "$pth" ] || continue 
+    (( `stat -c%s "$pth"` > 70000000 )) || continue # size > 70MB
+    
+    title=`basename "$pth" | perl -pe "s/(-| |,)/./g" | perl -pe "s~$pattern~~g" | perl -pe "s|\.+|.|g" | perl -pe "s/(\.|-)$//g"`
     link_folder=`echo "$folder" | sed "s|\/torrent\/|\/links\/|g"`
     link="$link_folder/$title.$ext"
 
@@ -19,7 +21,6 @@ media_group_links() {
 }
 
 alias_folder() {
-  # find . -type l | while read lk; do rm "$lk"; done
   media_group_links 'mkv' "$2" "$1"
   media_group_links 'avi' "$2" "$1"
   media_group_links 'mp4' "$2" "$1"
@@ -27,7 +28,7 @@ alias_folder() {
 }
 
 prep_dir() {
-  links='/mnt/bigboi/movingparts/links'
+  links='/mnt/movingparts/links'
   rm -rf "$links" || True
   mkdir "$links"
   mkdir "$links/TV"
@@ -37,10 +38,10 @@ prep_dir() {
 
 prep_dir
 
-find '/mnt/bigboi/movingparts/torrent/TV' -maxdepth 1 -mindepth 1  -type d \
+find '/mnt/movingparts/torrent/TV' -maxdepth 1 -mindepth 1  -type d \
   | while read pth; do alias_folder "$pth" '\d\d\d\d|'; done
-find '/mnt/bigboi/movingparts/torrent/Documentaries' -maxdepth 1 -mindepth 1  -type d \
+find '/mnt/movingparts/torrent/Documentaries' -maxdepth 1 -mindepth 1  -type d \
   | while read pth; do alias_folder "$pth"; done
-alias_folder '/mnt/bigboi/movingparts/torrent/Movies'
+alias_folder '/mnt/movingparts/torrent/Movies'
 # mnt/movingparts
 # find . -type l -exec cp --parents {} ../links \;
--- pi/scripts/mount_all.sh
+++ pi/scripts/mount_all.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 sudo mount -U "8790cc1e-e5ca-372e-b846-8cedd282e9bf" -t hfsplus -o force,rw /mnt/mbbackup && echo "mounted mbbackup"
 sudo mount -U "eea5b6a8-94a3-3fb8-99d8-3015bbafe289" -t hfsplus -o force,rw /mnt/usbhfs && echo "mounted usb"
-sudo mount -U "b3f3432a-57a0-4fb7-bb54-db528d15bca7" -t ext4 /mnt/seegayte && echo "mounted sg"
+sudo mount -U "b3f3432a-57a0-4fb7-bb54-db528d15bca7" -t ext4 /mnt/movingparts && echo "mounted mp 2TB"
 sudo mount -U "313cb347-2278-42d0-8a2c-f1dde1e82725" -t ext4 /mnt/bigboi && echo "mounted bb"
-sudo mount -U "40c2277a-91f3-440f-a9cb-8417e9d64e03" -t ext4 /mnt/movingparts && echo "mounted mp"
+sudo mount -U "40c2277a-91f3-440f-a9cb-8417e9d64e03" -t ext4 /mnt/seegayte && echo "mounted seegayte"
 
 
