2056ec27ee1af085d5f71e924ebe0965aa21d13f
Jake Iwen <you@example.com>
2021-02-17 05:50:07 -0700

    heyy

--- pi/scripts/rsync_schedule.sh
+++ pi/scripts/rsync_schedule.sh
@@ -7,7 +7,7 @@ BIGBOI_MOUNTED=$(mount | awk '/bigboi/ {print $6}' | grep "rw")
 [ ! $BIGBOI_MOUNTED ] && echo "bigboi not available/writable" && exit 0
 sudo rsync -avH --delete-during --delete-excluded --exclude-from=/rsync-exclude.txt / /mnt/bigboi/pi_backup_git/pi_backup
 
-cd /mnt/bigboi/pi_backup_git  || exit
+cd /mnt/bigboi/pi_backup_git  || exit 
 sudo git add .
 msg="Pi Backup - $(date)"
 sudo git commit -m "$msg"
