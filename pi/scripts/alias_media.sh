49e3d209b370f56eed480d1a83bbd6184b7bf970
Jake Iwen <you@example.com>
2021-03-09 15:02:32 -0700

    stash

--- pi/scripts/alias_media.sh
+++ pi/scripts/alias_media.sh
@@ -5,8 +5,8 @@ FILE_EXTENSIONS=(mkv avi mp4 r00)
 media_group_links() {
   regex="$2"
   folder="$1"
-  keys="${regex}multi|REQ|ETRG|YTM\.AM|SKGTV|CtrlHD|Will1869|10\.?Bit|DTS|DL|SDC|hdtv|EVO|WiKi|HMAX|IMAX|MA|VhsRip|iNTERNAL|True\.HD|1080p|720p|XviD|HD|AC3|AAC|REPACK|5\.1|2\.0|REMUX|PRiCK|AVC|HC|AMZN|HEVC|Bluray|(BR|web)(Rip)?|NF|DDP?(5\.1|2\.0)?|(x|h)\.?26[4-5]"
-  groups="d3g|CiNEFiLE|PRoDJi|regret|POIASD|Cinefeel|NTG|NTb|monkee|YELLOWBiRD|Atmos|EPSiLON|cielos|ION10|MeGusta|METCON|x0r|xlf|S8RHiNO|NTG|btx|strife|DD"
+  keys="${regex}multi|REQ|ETRG|YTM\.AM|SKGTV|CaLLiOpeD|CtrlHD|Will1869|10\.?Bit|DTS|DL|SDC|Atmos|hdtv|EVO|WiKi|HMAX|IMAX|MA|VhsRip|HDRip|BDRip|DD|iNTERNAL|True\.HD|1080p|1080i|720p|XviD|HD|AC3|AAC|REPACK|5\.1|2\.0|REMUX|PRiCK|AVC|HC|AMZN|HEVC|Bluray|(BR|web)(Rip)?|NF|DDP?(5\.1|2\.0)?|(x|h)\.?26[4-5]"
+  groups="d3g|CiNEFiLE|PRoDJi|regret|POIASD|Cinefeel|NTG|NTb|monkee|YELLOWBiRD|Atmos|EPSiLON|cielos|ION10|MeGusta|METCON|x0r|xlf|S8RHiNO|NTG|btx|strife|DD|DBS|TEPES|pawel2006"
   delims="\.|\+|\-"
   find "$folder" -not -path '*/\.*' -not -ipath '*sample*' -type f | while read pth
   do
@@ -14,12 +14,12 @@ media_group_links() {
     ext="${pth##*.}"
     [[ "${FILE_EXTENSIONS[*]}" =~ $ext ]] || continue # wrong extension
     (( `stat -c%s "$pth"` > 70000000 )) || continue # size > 70MB
-    pattern="($delims)(\[?($keys)\]?(?=\.)|(($groups)\.)?$ext$)|\'"
+    pattern="($delims)(\[?($keys)\]?(?=\.)|(($groups)\.)?\.?$ext$)|\'"
     # echo $pattern
     # echo "bname: `basename "$pth" | perl -pe "s/(-| |,)/./g"`"
-    title=`basename "$pth" | perl -pe "s/(-| |,)/./g" | perl -pe "s~$pattern~~ig"`
-    echo $title
-    link_folder=`echo "$folder" | sed "s|\/torrent\/|\/links\/|g" | sed "s| |\.|g"`
+    title=`basename "$pth" | perl -pe "s/(-| |,)/./g" | perl -pe "s~$pattern~~ig" | perl -pe "s~\.+~_~g"`
+    # echo $title
+    link_folder=`echo "$folder" | sed "s|\/torrent\/|\/links\/|g" | perl -pe "s~( |\.)+~_~g"`
     link="$link_folder/$title"
     
     [ -d "$link_folder" ] || mkdir "$link_folder"
@@ -27,27 +27,23 @@ media_group_links() {
   done
 }
 
-prep_dir() {
-  links="$SRC/links"
-  rm -rf "$links" || True
-  mkdir "$links"
-  mkdir "$links/TV"
-  mkdir "$links/Documentaries"
-  mkdir "$links/Movies"
-}
-
 alias_folders() {
-  find "$SRC/torrent/TV" -maxdepth 1 -mindepth 1  -type d \
+  src=$1
+  links="$src/links"
+  rm -rf "$links" || True
+  mkdir "$links" "$links/TV" "$links/Documentaries" "$links/Movies"
+  
+  find "$src/torrent/TV" -maxdepth 1 -mindepth 1  -type d \
     | while read pth; do media_group_links "$pth"; done
-  find "$SRC/torrent/Documentaries" -maxdepth 1 -mindepth 1  -type d \
+  find "$src/torrent/Documentaries" -maxdepth 1 -mindepth 1  -type d \
     | while read pth; do media_group_links "$pth"; done
-  media_group_links "$SRC/torrent/Movies"
+  media_group_links "$src/torrent/Movies"
+  
+  chmod -R 777 "$links"
 }
 
-# SRC="/mnt/bigboi/mp_backup"
-# prep_dir && alias_folders
-SRC="/mnt/movingparts"
-prep_dir && alias_folders
+[ -e "/mnt/movingparts" ] && alias_folders "/mnt/movingparts"
+[ -e "/mnt/bigboi/mp_backup" ] && alias_folders "/mnt/bigboi/mp_backup"
 
 
 # mnt/movingparts
