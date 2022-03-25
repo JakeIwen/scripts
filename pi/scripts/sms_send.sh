96e6df4034dae5390b6ec79c58ffe7c113481712
Jake Iwen <iwenx006@umn.edu>
2022-03-25 09:41:29 -0600

    'min rate'

--- pi/scripts/sms_send.sh
+++ pi/scripts/sms_send.sh
@@ -2,4 +2,10 @@
 
 source /home/pi/.twilio/twilio_creds.sh
 url="https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages"
-curl -X POST -d "Body=$1" -d "From=$TWILIO_NUMBER" -d "To=$TO_NUMBER" $url -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
+# curl -X POST -d "Body=$1" -d "From=$TWILIO_NUMBER" -d "To=$TO_NUMBER" $url -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
+
+curl -X POST $url \
+  --data-urlencode "To=$TO_NUMBER" \
+  --data-urlencode "From=$TWILIO_NUMBER" \
+  --data-urlencode "Body=$(echo -e $1)" \
+  -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
