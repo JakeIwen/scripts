4e4b6750ce99abbcaba76b0cfaf301c942be69ff
Jake Iwen <iwenx006@umn.edu>
2022-04-27 02:20:43 -0600

    prestash

--- vanrouter/root/auto_dns.sh
+++ vanrouter/root/auto_dns.sh
@@ -2,15 +2,10 @@
 
 # The script automatically switches the DNS servers between Pi-hole and Cloudflare based on Pi-hole DNS Server status.
 
-logfile="/root/log/auto_dns.log"
+# logfile="/root/log/auto_dns.log"
 PIHOLE=192.168.6.103 # Pi-hole
 FALLBACK_A=1.1.1.1 # Cloudflare
 FALLBACK_B=1.0.0.1 # Cloudflare
-
-DNS_CFG=$(uci show dhcp.@dnsmasq[0].server)
-PIHOLE_PING_CT=$(ping -c 1 -w 1 $PIHOLE | grep seq | wc -l)
-PIHOLE_DNS_CT=$(echo $DNS_CFG | grep $PIHOLE | wc -l)
-FALLBACK_DNS_CT=$(echo $DNS_CFG | grep $FALLBACK_A | wc -l)
 
 # delete_nth_from_last_line() {
 #   lines=$(cat "$1" | wc -l)
@@ -18,14 +13,14 @@ FALLBACK_DNS_CT=$(echo $DNS_CFG | grep $FALLBACK_A | wc -l)
 #   sed -i "${target}d" "$1"
 # }
 
-delete_last_line() {
-  lines=$(cat "$1" | wc -l)
-  sed -i "${lines}d" "$1"
-}
-
-delete_first_line() {
-  sed -i '1d' "$1"
-}
+# delete_last_line() {
+#   lines=$(cat "$1" | wc -l)
+#   sed -i "${lines}d" "$1"
+# }
+# 
+# delete_first_line() {
+#   sed -i '1d' "$1"
+# }
 
 echo_dns_data() {
   echo "$(date)"
@@ -46,15 +41,30 @@ set_dns() {
   /etc/init.d/dnsmasq restart
 }
 
-if [ $PIHOLE_PING_CT -eq 1 ] && [ $PIHOLE_DNS_CT -eq 0 ]; then
-  echo "Setting target DNS server to pi: $PIHOLE"
-  set_dns $PIHOLE
-elif [ $PIHOLE_PING_CT -eq 0 ] && [ $FALLBACK_DNS_CT -eq 0 ]; then # && ping -c 1 $FALLBACK_A > /dev/null
-  echo "Setting fallback DNS servers $FALLBACK_A $FALLBACK_B"
-  set_dns $FALLBACK_A $FALLBACK_B
-else
-  while [ "$(cat $logfile | wc -l)" -gt 50 ]; do
-    delete_first_line "$logfile"
-  done
-  echo "$(date) unchanged"
-fi
+run_auto() {
+  DNS_CFG=$(uci show dhcp.@dnsmasq[0].server)
+  PIHOLE_PING_CT=$(ping -c 1 -w 1 $PIHOLE | grep seq | wc -l)
+  PIHOLE_DNS_CT=$(echo $DNS_CFG | grep $PIHOLE | wc -l)
+  FALLBACK_DNS_CT=$(echo $DNS_CFG | grep $FALLBACK_A | wc -l)
+  
+  if [ $PIHOLE_PING_CT -eq 1 ] && [ $PIHOLE_DNS_CT -eq 0 ]; then
+    echo "Setting target DNS server to pi: $PIHOLE"
+    set_dns $PIHOLE
+  elif [ $PIHOLE_PING_CT -eq 0 ] && [ $FALLBACK_DNS_CT -eq 0 ]; then # && ping -c 1 $FALLBACK_A > /dev/null
+    echo "Setting fallback DNS servers $FALLBACK_A $FALLBACK_B"
+    set_dns $FALLBACK_A $FALLBACK_B
+  # else
+  #   while [ "$(cat $logfile | wc -l)" -gt 50 ]; do
+  #     delete_first_line "$logfile"
+  #   done
+  #   echo "$(date) unchanged"
+  fi
+}
+
+while true; do
+	run_auto
+	sleep 30
+done
+
+
+exit 0
