98f901f667ab82e22a8d6755a80194024677ae19
Jake Iwen <iwenx006@umn.edu>
2022-04-27 02:20:43 -0600

    prestash

--- sync_scripts.sh
+++ sync_scripts.sh
@@ -13,29 +13,30 @@ mkdir "$scripts/python/"
 find "$dsc/automation/" -type f -name "*.py" -exec cp {} "$scripts/python/" \;
 
 # RASPI
-scp  "$dsc/pi/.bashrc" "$pi_ip:/home/pi/.bashrc"
-scp  "$dsc/pi/sns.sh"  "$pi_ip:/home/pi/sns.sh"
+scp  "$dsc/pi/.bashrc" "$pi_ip:/home/pi/.bashrc" &
+scp  "$dsc/pi/sns.sh"  "$pi_ip:/home/pi/sns.sh" &
 
-scp -r "$scripts" "$pi_ip:/home/pi/"
-scp -r "$hooks" "$pi_ip:/home/pi/"
-scp -r "$twilio" "$pi_ip:/home/pi/"
+scp -r "$scripts" "$pi_ip:/home/pi/" &
+scp -r "$hooks" "$pi_ip:/home/pi/" &
+scp -r "$twilio" "$pi_ip:/home/pi/" &
   
-scp  "$dsc/pi/NativCast/process.py"  "$pi_ip:/home/pi/NativCast/process.py"
-scp  "$dsc/pi/NativCast/server.py"  "$pi_ip:/home/pi/NativCast/server.py"
+scp  "$dsc/pi/NativCast/process.py"  "$pi_ip:/home/pi/NativCast/process.py" &
+scp  "$dsc/pi/NativCast/server.py"  "$pi_ip:/home/pi/NativCast/server.py" &
 
 # configs
-scp  "$configs/smb.conf" "$pi_ip:/etc/samba/smb.conf"
-scp  "$configs/.bash_defaults" "$pi_ip:/home/pi/.bash_defaults"
-scp  "$configs/.mount_aliases" "$pi_ip:/home/pi/.mount_aliases"
-scp  "$configs/.disk_uuids" "$pi_ip:/home/pi/.disk_uuids"
-scp  "$configs/rsync-exclude.txt" "$pi_ip:/rsync-exclude.txt"
-scp  "$configs/rsync-exclude-media.txt" "$pi_ip:/rsync-exclude-media.txt"
-
-# CLEANUP
-rm -rf $scripts/python/
+scp  "$configs/smb.conf" "$pi_ip:/etc/samba/smb.conf" &
+scp  "$configs/.bash_defaults" "$pi_ip:/home/pi/.bash_defaults" &
+scp  "$configs/.mount_aliases" "$pi_ip:/home/pi/.mount_aliases" &
+scp  "$configs/.disk_uuids" "$pi_ip:/home/pi/.disk_uuids" &
+scp  "$configs/rsync-exclude.txt" "$pi_ip:/rsync-exclude.txt" &
+scp  "$configs/rsync-exclude-media.txt" "$pi_ip:/rsync-exclude-media.txt" &
 
 # ROUTER
-# scp -r "$vr_ip:/etc/config" "$vanrouter/etc/"
-scp "$vanrouter/root/auto_dns.sh" "$vr_ip:/root/auto_dns.sh"
-scp "$vanrouter/root/.profile" "$vr_ip:/root/.profile"
+scp -r "$vr_ip:/etc/config" "$vanrouter/etc/" &
+scp "$vanrouter/root/auto_dns.sh" "$vr_ip:/root/auto_dns.sh" &
+scp "$vanrouter/root/.profile" "$vr_ip:/root/.profile" &
+# scp "$vanrouter/root/dnsmasq.awk" "$vr_ip:/root/dnsmasq.awk" &
 
+# CLEANUP
+sleep 4
+rm -rf $scripts/python/
