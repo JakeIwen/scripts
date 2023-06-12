a7f90b8d39037b719c22e777d12bccb8a9873f31
Jake Iwen <iwenx006@umn.edu>
2023-06-12 11:05:26 -0600

    stash

--- pi/scripts/log_position.sh
+++ pi/scripts/log_position.sh
@@ -2,13 +2,17 @@
 
 POSPATH="/home/pi/vlc-positions.txt"
 
+uridecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; } 
+
 [[ -z "$(pgrep vlc)" ]] && exit 0
 file=`python /home/pi/scripts/python/vlc_property.py URL`
 nsecs=`python /home/pi/scripts/python/vlc_property.py NS`
 if [[ $file && $nsecs ]]; then
-  sed -i "\|^$file|d" $POSPATH
-  echo "$file $nsecs" >> $POSPATH
-  echo "$file $nsecs"
+  decoded="`uridecode $file`"
+  trimmed="`echo $decoded | sed -E 's|.*\/links||g'`"
+  sed -i "\|^$trimmed|d" $POSPATH
+  echo "$trimmed $nsecs" >> $POSPATH
+  echo "$trimmed $nsecs"
 else
   echo "could not extract position on file: $file"
 fi
